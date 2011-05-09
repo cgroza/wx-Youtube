dce96a7311a936245695c5ac4a6915be8b05de7c
Afterburn <stealth@aft3rburn.info>
2011-05-09 12:10:31 -0700

    Created a error message box
    Created a error message box incase curl fails, it simply tells you to
    check your internet connection.

--- src/wx-youtube.cpp
+++ src/wx-youtube.cpp
@@ -104,8 +104,7 @@ std::vector<VideoInfo*>* get_search_result(wxString& search)  //had help with th
 	}
 	else
 	{
-	    //Needs to be a popup message
-	    std::cout << "Error [" << result << "] - " << errorBuffer << std::endl;
+	    wxMessageBox(_("Curl has had an error, are you connected to the internet?"),_("Error"), wxOK | wxICON_INFORMATION);
 	    return 0;
 	}
     }
